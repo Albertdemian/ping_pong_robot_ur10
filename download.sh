2f84ed524de9885cf1e1097d3b2f5c28293790b9
sevocrear <sevocrear@gmail.com>
2020-06-19 12:29:51 +0300

    updated the file in order to except the error about not having /opt/pylon5

--- download.sh
+++ download.sh
@@ -4,6 +4,10 @@ sudo apt-get install -y swig
 wget https://www.baslerweb.com/fp-1589378344/media/downloads/software/pylon_software/pylon_6.1.1.19861-deb0_amd64.deb
 sudo apt-get install ./pylon_6.1.1.19861-deb0_amd64.deb
 sudo apt-get install python3-pip
+git clone https://github.com/basler/pypylon.git
+cd pypylon
+sed -i '536s/pylon5/pylon/' setup.py # if it given an error like you don't have /opt/pylon5
+pip3 install .
 #Create output file, override if already present  
 output=requirements.txt 
 echo "numpy
