be77572cb698b87337deae5e8d8f1c20b5bb9ead
sevocrear <sevocrear@gmail.com>
2020-06-19 12:09:53 +0300

    updated download.sh file. Just type ./download.sh

--- download.sh
+++ download.sh
@@ -1,5 +1,17 @@
-cd ~/Downloads/
-sudo apt-get update -y
-sudo apt-get install -y swig
-wget https://www.baslerweb.com/fp-1589378344/media/downloads/software/pylon_software/pylon_6.1.1.19861-deb0_amd64.deb
-sudo apt-get install ./pylon_6.1.1.19861-deb0_amd64.deb
+# cd ~/Downloads/
+# sudo apt-get update -y
+# sudo apt-get install -y swig
+# wget https://www.baslerweb.com/fp-1589378344/media/downloads/software/pylon_software/pylon_6.1.1.19861-deb0_amd64.deb
+# sudo apt-get install ./pylon_6.1.1.19861-deb0_amd64.deb
+# sudo apt-get install python3-pip
+#Create output file, override if already present  
+output=requirements.txt 
+echo "numpy
+pylon
+matplotlib
+imutils
+opencv-python
+rospkg
+" > $output
+pip3 install -r requirements.txt
+rm -f requirements.txt
