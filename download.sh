08b3e7f5a2422dacc6eefe2237639f236a4d4ee7
sevocrear <sevocrear@gmail.com>
2020-06-19 12:00:22 +0300

    added bash and requirements file in order to work with the camera

---
+++ download.sh
@@ -0,0 +1,5 @@
+cd ~/Downloads/
+sudo apt-get update -y
+sudo apt-get install -y swig
+wget https://www.baslerweb.com/fp-1589378344/media/downloads/software/pylon_software/pylon_6.1.1.19861-deb0_amd64.deb
+sudo apt-get install ./pylon_6.1.1.19861-deb0_amd64.deb
