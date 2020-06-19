62d4a8fea9b72a7b3b484a6b8b4ededa25f93c3b
sevocrear <sevocrear@gmail.com>
2020-06-19 12:11:30 +0300

    updated download.sh file. Just type ./download.sh

--- download.sh
+++ download.sh
@@ -1,9 +1,9 @@
-# cd ~/Downloads/
-# sudo apt-get update -y
-# sudo apt-get install -y swig
-# wget https://www.baslerweb.com/fp-1589378344/media/downloads/software/pylon_software/pylon_6.1.1.19861-deb0_amd64.deb
-# sudo apt-get install ./pylon_6.1.1.19861-deb0_amd64.deb
-# sudo apt-get install python3-pip
+cd ~/Downloads/
+sudo apt-get update -y
+sudo apt-get install -y swig
+wget https://www.baslerweb.com/fp-1589378344/media/downloads/software/pylon_software/pylon_6.1.1.19861-deb0_amd64.deb
+sudo apt-get install ./pylon_6.1.1.19861-deb0_amd64.deb
+sudo apt-get install python3-pip
 #Create output file, override if already present  
 output=requirements.txt 
 echo "numpy
